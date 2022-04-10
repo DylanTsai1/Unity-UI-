8f410616e0349f522edd38d75a33ef00580c422f
DylanTsai1 <101568962+DylanTsai1@users.noreply.github.com>
2022-04-09 21:26:23 -0300

    Update VariedadContraseña.cs

--- Assets/Scripts/VariedadContraseña.cs
+++ Assets/Scripts/VariedadContraseña.cs
@@ -1,13 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class VariedadContraseña : MonoBehaviour
 {
+    string contraseniaCorrecta;
+    string contraseniaUsuario;
+    Text ingresoUsuario;    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        contraseniaCorrecta = "12345"; 
+
+
     }
 
     // Update is called once per frame
