8ca8f5e2bbaf3cc9e70f4ceca9c4a106c9dd3618
DylanTsai1 <101568962+DylanTsai1@users.noreply.github.com>
2022-04-10 01:34:24 -0300

    Video 6 Terminado

--- Assets/Scripts/VariedadContraseña.cs
+++ Assets/Scripts/VariedadContraseña.cs
@@ -7,13 +7,14 @@ public class VariedadContraseña : MonoBehaviour
 {
     string contraseniaCorrecta;
     string contraseniaUsuario;
-    public Text ingresoUsuario;    
-
+    public Text ingresoUsuario;
+    public GameObject cartelitoMsj;
+    public Text textoMsj;
     // Start is called before the first frame update
     void Start()
     {
         contraseniaCorrecta = "12345"; 
-
+        cartelitoMsj.SetActive(false);
 
     }
 
@@ -29,10 +30,14 @@ public class VariedadContraseña : MonoBehaviour
         contraseniaUsuario = ingresoUsuario.text;
         if (contraseniaUsuario == contraseniaCorrecta)
         {
+            cartelitoMsj.SetActive(true);
+            textoMsj.text = "Bienvenido";
             Debug.Log("Bienvenido");
         }
         else
         {
+            cartelitoMsj.SetActive(true);
+            textoMsj.text = "Contraseña incorrecta";
             Debug.Log("Contraseña incorrecta");
 
         }
